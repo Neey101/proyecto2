5ff66970893239c5104450c8f9776df71c378ef4
Neythan Candia <neeycandia1001@gmail.com>
2023-09-04 11:11:57 -0300

    cambio de colores

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,16 +29,11 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-
     from . import db
     db.init_app(app)
     
     from . import auth
     app.register_blueprint(auth.bp)
-
-def create_app():
-    app = ...
-    # existing code omitted
 
     from . import blog
     app.register_blueprint(blog.bp)
